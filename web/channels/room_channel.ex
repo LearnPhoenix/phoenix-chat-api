58ae30e328da017bc263899519ba51ce7d41d66e
Sam Corcos <sam@corcos.io>
2016-09-14 07:26:04 -0400

    set up phoenix channels

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,7 +1,8 @@
 defmodule PhoenixChat.RoomChannel do
   use PhoenixChat.Web, :channel
+  require Logger
 
-  def join("room:lobby", payload, socket) do
+  def join("room:" <> _uid, payload, socket) do
     if authorized?(payload) do
       {:ok, socket}
     else
@@ -9,16 +10,9 @@ defmodule PhoenixChat.RoomChannel do
     end
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (room:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+  def handle_in("message", payload, socket) do
+    Logger.debug "#{inspect payload}"
+    broadcast socket, "message", payload
     {:noreply, socket}
   end
 
