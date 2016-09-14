a3a7f3b71e2055ec34bb1f40342fc675d254e472
Sam Corcos <sam@corcos.io>
2016-09-14 06:12:44 -0400

    login and authentication part 1

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,11 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    identity: {
+      Ueberauth.Strategy.Identity,
+      [callback_methods: ["POST"]]
+    }
+  ]
