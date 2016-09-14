7ccc5d7b3ad7daca05c21f34a487eb3ec6f25c2a
Sam Corcos <sam@corcos.io>
2016-09-14 06:19:43 -0400

    login and authentication part 2

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,8 +1,24 @@
 defmodule PhoenixChat.AuthController do
   use PhoenixChat.Web, :controller
 
-  def test(conn, _params) do
-    IO.puts "AuthController called!"
-    conn
+  alias PhoenixChat.{ErrorView, UserView, User, AuthController}
+
+  plug Ueberauth
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    result = with {:ok, user} <- user_from_auth(auth),
+                  :ok <- validate_pass(user.encrypted_password, auth.credentials.other.password),
+                  do: signin_user(conn, user)
+
+    case result do
+      {:ok, user, token} ->
+        conn
+        |> put_status(:created)
+        |> render(UserView, "show.json", user: user, token: token)
+      {:error, reason} ->
+        conn
+        |> put_status(:bad_request)
+        |> render(ErrorView, "error.json", error: reason)
+    end
   end
 end
