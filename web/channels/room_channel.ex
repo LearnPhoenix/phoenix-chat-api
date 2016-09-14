5092de1aa09fadad35618e697cba1839f0b6ff4f
Sam Corcos <sam@corcos.io>
2016-09-14 07:27:44 -0400

    set up phoenix channels

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -3,21 +3,14 @@ defmodule PhoenixChat.RoomChannel do
   require Logger
 
   def join("room:" <> _uid, payload, socket) do
-    if authorized?(payload) do
+    authorize(payload, fn ->
       {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+    end)
   end
 
   def handle_in("message", payload, socket) do
     Logger.debug "#{inspect payload}"
     broadcast socket, "message", payload
     {:noreply, socket}
-  end
-
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
   end
 end
