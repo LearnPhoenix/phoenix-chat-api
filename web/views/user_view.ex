83939f0bc3758f2b83bd8ada46508432a6c5febc
Sam Corcos <sam@corcos.io>
2016-09-14 06:20:32 -0400

    login and authentication part 2

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,17 +1,30 @@
 defmodule PhoenixChat.UserView do
   use PhoenixChat.Web, :view
 
+  alias PhoenixChat.{UserView}
+
   def render("index.json", %{users: users}) do
-    %{data: render_many(users, PhoenixChat.UserView, "user.json")}
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user, token: token}) do
+    %{data: render_one(user, UserView, "user_token.json", token: token)}
   end
 
   def render("show.json", %{user: user}) do
-    %{data: render_one(user, PhoenixChat.UserView, "user.json")}
+    %{data: render_one(user, UserView, "user.json")}
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id,
-      email: user.email,
+    %{email: user.email,
+      id: user.id,
+      username: user.username}
+  end
+
+  def render("user_token.json", %{user: user, token: token}) do
+    %{email: user.email,
+      id: user.id,
+      token: token,
       username: user.username}
   end
 end
