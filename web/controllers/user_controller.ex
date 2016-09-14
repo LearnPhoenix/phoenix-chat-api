05b08a1bca721fb76a35346cd7325c135b4aa5d8
Sam Corcos <sam@corcos.io>
2016-09-14 05:40:06 -0400

    user accounts and signup part 2

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,19 +3,13 @@ defmodule PhoenixChat.UserController do
 
   alias PhoenixChat.User
 
-  def index(conn, _params) do
-    users = Repo.all(User)
-    render(conn, "index.json", users: users)
-  end
-
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
         |> put_status(:created)
-        |> put_resp_header("location", user_path(conn, :show, user))
         |> render("show.json", user: user)
       {:error, changeset} ->
         conn
@@ -23,12 +17,7 @@ defmodule PhoenixChat.UserController do
         |> render(PhoenixChat.ChangesetView, "error.json", changeset: changeset)
     end
   end
-
-  def show(conn, %{"id" => id}) do
-    user = Repo.get!(User, id)
-    render(conn, "show.json", user: user)
-  end
-
+  
   def update(conn, %{"id" => id, "user" => user_params}) do
     user = Repo.get!(User, id)
     changeset = User.changeset(user, user_params)
