ba2699bd623c223e5869cbd050e293766e6987ef
Gabriel Jaldon <gjaldon85@gmail.com>
2016-09-16 16:50:12 +0800

    Ensure that anonymous user is saved.
    When an anonymous user joins admin:active_users topic, we
    make sure that user is saved to our DB. Also, we no longer
    track the presence of admins to simplify our code especially
    in the frontend (no more filtering out admin's id).
    
    We also add a simple validation on the params passed on socket
    connect. We raise an error if both id and uuid params are
    empty. That way, code that gets executed in channels can expect
    that a socket.assigns will always have a non-empty user_id
    or uuid. This helps to simplify our code too and informs
    the frontend that an id or uuid param is expected.

--- web/channels/admin_channel.ex
+++ web/channels/admin_channel.ex
@@ -1,12 +1,12 @@
 defmodule PhoenixChat.AdminChannel do
   @moduledoc """
-  The channel used to give the administrator access to all users.
+  The channel used to give the administrator access to all users.
   """
 
   use PhoenixChat.Web, :channel
   require Logger
 
-  alias PhoenixChat.{Presence}
+  alias PhoenixChat.{Presence, Repo, AnonymousUser}
 
   @doc """
   The `admin:active_users` topic is how we identify all users currently using the app.
@@ -14,20 +14,40 @@ defmodule PhoenixChat.AdminChannel do
   def join("admin:active_users", payload, socket) do
     authorize(payload, fn ->
       send(self, :after_join)
-      {:ok, socket}
+      id = socket.assigns[:uuid] || socket.assigns[:user_id]
+      {:ok, %{id: id}, socket}
     end)
   end
 
   @doc """
-  This handles the `:after_join` event and tracks the presence of the socket that has subscribed to the `admin:active_users` topic.
+  This handles the `:after_join` event and tracks the presence of the socket that
+  has subscribed to the `admin:active_users` topic.
   """
-  def handle_info(:after_join, socket) do
+  # We no longer track admin's presence. This will lead to simpler code in the
+  # frontend by not having to filter out admin from the list of users in the Sidebar.
+  def handle_info(:after_join, %{assigns: %{user_id: _user_id}} = socket) do
+    {:noreply, socket}
+  end
+
+  # We track only the presence of anonymous users and ensure they are stored
+  # in our DB.
+  def handle_info(:after_join, %{assigns: %{uuid: uuid}} = socket) do
+    # We save anonymous user to DB when it hasn't been saved before
+    ensure_user_saved!(uuid)
+
     push socket, "presence_state", Presence.list(socket)
     Logger.debug "Presence for socket: #{inspect socket}"
-    id = socket.assigns.user_id || socket.assigns.uuid
-    {:ok, _} = Presence.track(socket, id, %{
+    {:ok, _} = Presence.track(socket, uuid, %{
       online_at: inspect(System.system_time(:seconds))
     })
     {:noreply, socket}
+  end
+
+  defp ensure_user_saved!(uuid) do
+    user_exists = Repo.get(AnonymousUser, uuid)
+    unless user_exists do
+      changeset = AnonymousUser.changeset(%AnonymousUser{}, %{id: uuid})
+      Repo.insert!(changeset)
+    end
   end
 end
