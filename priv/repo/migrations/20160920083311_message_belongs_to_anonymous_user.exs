13999a1399e4a05539943ab949952f33defcc83f
Sam Corcos <sam@corcos.io>
2016-09-20 04:34:42 -0400

    create anonymous user model

--- priv/repo/migrations/20160920083311_message_belongs_to_anonymous_user.exs
+++ priv/repo/migrations/20160920083311_message_belongs_to_anonymous_user.exs
@@ -1,7 +1,17 @@
 defmodule PhoenixChat.Repo.Migrations.MessageBelongsToAnonymousUser do
   use Ecto.Migration
 
-  def change do
+  def up do
+    alter table(:messages) do
+      add :anonymous_user_id, references(:anonymous_users, on_delete: :nilify_all, type: :uuid)
+      remove :from
+    end
+  end
 
+  def down do
+    alter table(:messages) do
+      remove :anonymous_user_id
+      add :from, :string
+    end
   end
 end
