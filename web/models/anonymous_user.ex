bebe0a65a408490837a2eb579aadea6e2e672e52
Sam Corcos <sam@corcos.io>
2016-09-20 04:35:19 -0400

    create anonymous user model

--- web/models/anonymous_user.ex
+++ web/models/anonymous_user.ex
@@ -20,4 +20,17 @@ defmodule PhoenixChat.AnonymousUser do
     |> put_avatar
     |> put_name
   end
+
+  defp put_name(changeset) do
+    adjective = Faker.Color.name |> String.capitalize
+    noun = Faker.Company.buzzword_suffix |> String.capitalize
+    name = adjective <> " " <> noun
+    changeset
+    |> put_change(:name, name)
+  end
+
+  defp put_avatar(changeset) do
+    changeset
+    |> put_change(:avatar, Faker.Avatar.image_url(25, 25))
+  end
 end
